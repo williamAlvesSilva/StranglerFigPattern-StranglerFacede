58f94f032ffe9f56bd5673ecea4fe033059fadea
williamAlvesSilva <156724304+williamAlvesSilva@users.noreply.github.com>
2025-02-24 02:03:29 -0300

    Add files via upload

---
+++ StraglerFacedeApplication.java
@@ -0,0 +1,13 @@
+package com.stragler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StraglerFacedeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StraglerFacedeApplication.class, args);
+	}
+
+}
